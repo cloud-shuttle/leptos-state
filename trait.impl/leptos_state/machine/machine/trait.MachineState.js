9baab00fa8d48318d3caebbe34c4b44794977d5e
petehanssens <petehanssens@users.noreply.github.com>
2025-09-27 11:07:05 +0000

    deploy: f416dfb836e1715216ac037a87accf5682f29954

--- trait.impl/leptos_state/machine/machine/trait.MachineState.js
+++ trait.impl/leptos_state/machine/machine/trait.MachineState.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["leptos_state",[]],["traffic_light_example",[["impl MachineState for <a class=\"struct\" href=\"traffic_light_example/struct.TrafficMachineState.html\" title=\"struct traffic_light_example::TrafficMachineState\">TrafficMachineState</a>"]]]]);
+    var implementors = Object.fromEntries([["leptos_state",[]],["traffic_light_example",[["impl <a class=\"trait\" href=\"leptos_state/machine/machine/trait.MachineState.html\" title=\"trait leptos_state::machine::machine::MachineState\">MachineState</a> for <a class=\"struct\" href=\"traffic_light_example/struct.TrafficMachineState.html\" title=\"struct traffic_light_example::TrafficMachineState\">TrafficMachineState</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[19,221]}
+//{"start":57,"fragment_lengths":[19,367]}
--- trait.impl/leptos_state/machine/machine/trait.StateMachine.js
+++ trait.impl/leptos_state/machine/machine/trait.StateMachine.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["traffic_light_example",[["impl StateMachine for <a class=\"struct\" href=\"traffic_light_example/struct.TrafficMachine.html\" title=\"struct traffic_light_example::TrafficMachine\">TrafficMachine</a>"]]]]);
+    var implementors = Object.fromEntries([["traffic_light_example",[["impl <a class=\"trait\" href=\"leptos_state/machine/machine/trait.StateMachine.html\" title=\"trait leptos_state::machine::machine::StateMachine\">StateMachine</a> for <a class=\"struct\" href=\"traffic_light_example/struct.TrafficMachine.html\" title=\"struct traffic_light_example::TrafficMachine\">TrafficMachine</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[205]}
+//{"start":57,"fragment_lengths":[351]}
